5a5116b0eeb3873d0976371bce3b307c9f4cec64
andresaleksandarmilutin@gmail.com <Franch98>
2021-07-22 18:21:11 -0500

    Listado de todos los Proveedores

---
+++ MVC_Hunde/Controllers/ProveedorController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVC_Hunde.ProxyProveedor;
+
+namespace MVC_Hunde.Controllers
+{
+
+    public class ProveedorController : Controller
+    {
+        ServicioProveedorClient servicioProveedor = new ServicioProveedorClient();
+
+        // GET: Proveedor
+        public ActionResult Index()
+        {
+            ViewBag.ListarProveedores = servicioProveedor.ConsultarProveedores();
+            return View();
+        }
+    }
+}
